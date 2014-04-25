5b16303240caa5c173c2d9ba2ab7c93ca6ff4982
OpenShift guest <ge.unitel@gmail.com>
2014-04-25 10:39:55 -0400

    some stuffs

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+	//'url' : 'your-settings-here' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+'url': "mongodb://george:orange1@oceanic.mongohq.com:10067/testing"
+};
